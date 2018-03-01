c3de231f839b1df15a8b0278535523537ec94b40
Renars Kudins <renars.kudins@gmail.com>
2018-03-01 20:55:56 +0200

    Hashcode 2018

---
+++ src/main/scala/hashcode/Main.scala
@@ -0,0 +1,7 @@
+package hashcode
+
+object Main
+  extends App {
+  val domain = WorkWithFile("a_example.in").toDomain
+  println(domain)
+}
---
+++ src/main/scala/hashcode/WorkWithFile.scala
@@ -0,0 +1,51 @@
+package hashcode
+
+import java.net.URI
+
+import scala.io.Source
+import scala.util.Try
+
+class WorkWithFile(fileName: String) {
+  val file: URI = getClass.getClassLoader.getResource(fileName).toURI
+
+  val toDomain: Option[(Metadata, List[Ride])] = Try {
+    val (first :: rest) = Source.fromURI(file).getLines().toList
+    val Array(r, c, f, n, b, t) = first.split(" ").map(_.toInt)
+    val m = Metadata(r, c, f, n, b, t)
+    val rides = rest.map { r =>
+      val Array(a, b, x, y, s, f) = r.split(" ").map(_.toInt)
+      Ride(
+        start = Intersection(a, b),
+        end = Intersection(x, y),
+        s = s,
+        f = f
+      )
+    }
+    (m, rides)
+  }.toOption
+}
+
+case class Metadata(
+  R: Int,
+  C: Int,
+  F: Int,
+  N: Int,
+  B: Int,
+  T: Int
+)
+
+case class Intersection(
+  x: Int,
+  y: Int
+)
+
+case class Ride(
+  start: Intersection,
+  end: Intersection,
+  s: Int,
+  f: Int
+)
+
+object WorkWithFile {
+  def apply(fileName: String): WorkWithFile = new WorkWithFile(fileName)
+}
